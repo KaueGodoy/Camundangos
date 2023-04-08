5d1a11f5066ff599c8b8b8d993f6625d8725e85a
Kaue Godoy <kauegodoy@live.com>
2023-04-07 23:42:21 -0300

    Platforms+EnemyFlip

--- Roguelike/Assets/Scripts/Enemy/SimpleAI/EnemyFollow.cs
+++ Roguelike/Assets/Scripts/Enemy/SimpleAI/EnemyFollow.cs
@@ -9,6 +9,12 @@ public class EnemyFollow : MonoBehaviour
     public float minimunDistance = 10f;
     public float moveSpeed = 2f;
 
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void Update()
     {
@@ -26,6 +32,29 @@ public class EnemyFollow : MonoBehaviour
 
     private void Chase()
     {
+        
+        //transform.LookAt(target.position);
+
+        // Calculate the direction to the player
+        Vector2 direction = (target.position - transform.position).normalized;
+
+        // Calculate the angle between the current position and the player
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // Set the rotation of the enemy to face the player
+        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        
+
+        // Flip the sprite horizontally depending on the player's position
+        if (direction.x > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+
         transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
     }
 
