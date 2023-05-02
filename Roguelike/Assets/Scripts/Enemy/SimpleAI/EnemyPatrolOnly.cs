9017ace707123dde35c51365b5ed535304c0d287
Kaue Godoy <kauegodoy@live.com>
2023-05-01 22:41:22 -0300

    FishKnockback

--- Roguelike/Assets/Scripts/Enemy/SimpleAI/EnemyPatrolOnly.cs
+++ Roguelike/Assets/Scripts/Enemy/SimpleAI/EnemyPatrolOnly.cs
@@ -16,9 +16,17 @@ public class EnemyPatrolOnly : MonoBehaviour
     public float attackRange = 5f;
     public float damage = 300f;
 
+    [Header("Knockback")]
+    public float knockbackForce = 10f;
+    public float knockbackDuration = 0.5f;
+
+    public bool isKnockback = false;
+    public float knockbackTimer = 0f;
+
     [Header("Patrol")]
     public Transform[] patrolPoints;
     private int patrolIndex = 0;
+
 
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rb;
@@ -34,6 +42,24 @@ public class EnemyPatrolOnly : MonoBehaviour
     {
         FlipSprite();
         Patrol();
+    }
+
+
+    private void FixedUpdate()
+    {
+        CheckTimers();
+    }
+
+    private void CheckTimers()
+    {
+        if (isKnockback)
+        {
+            knockbackTimer -= Time.fixedDeltaTime;
+            if (knockbackTimer <= 0f)
+            {
+                isKnockback = false;
+            }
+        }
     }
 
     private void Patrol()
@@ -71,12 +97,36 @@ public class EnemyPatrolOnly : MonoBehaviour
         DamagePopup.Create(player.transform.position + Vector3.right + Vector3.up, (int)damage);
     }
 
+    private void ApplyKnockback()
+    {
+        Rigidbody2D playerRigidbody = player.GetComponent<Rigidbody2D>();
+        Vector2 direction = player.gameObject.transform.position - transform.position;
+        playerRigidbody.velocity = direction.normalized * knockbackForce;
+        isKnockback = true;
+        knockbackTimer = knockbackDuration;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // rigid body velocity
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PerformAttack();
+            ApplyKnockback();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // rigid body impulse (based on mass)
         if (player != null)
         {
             player.TakeDamage(damage);
             DamagePopup.Create(player.transform.position + Vector3.right + Vector3.up, (int)damage);
+
+            Rigidbody2D playerRb = player.GetComponent<Rigidbody2D>();
+            Vector3 direction = collision.gameObject.transform.position - transform.position;
+            playerRb.AddForce(direction.normalized * knockbackForce, ForceMode2D.Impulse);
         }
     }
 
