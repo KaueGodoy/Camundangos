c4a20e66c0268ffd7467ad76a2e7e6d76f426408
Kaue Godoy <kauegodoy@live.com>
2023-05-03 18:24:30 -0300

    UltUI

--- Roguelike/Assets/Scripts/UI/HUD/PlayerCooldowns.cs
+++ Roguelike/Assets/Scripts/UI/HUD/PlayerCooldowns.cs
@@ -8,6 +8,7 @@ using static UnityEngine.GraphicsBuffer;
 
 public class PlayerCooldowns : MonoBehaviour
 {
+    [Header("Skil")]
     [SerializeField] private TextMeshProUGUI cooldownText;
     [SerializeField] private Image foregroundImage;
     [SerializeField] private Image iconImage;
@@ -22,10 +23,30 @@ public class PlayerCooldowns : MonoBehaviour
     Color cooldownColor = new Color32(164, 164, 164, 255);
     Color defaultColor = new Color32(255, 255, 255, 168);
 
+    // ult
+    [Header("Ult")]
+    [SerializeField] private TextMeshProUGUI ultCooldownText;
+    [SerializeField] private Image ultForegroundImage;
+    [SerializeField] private Image ultIconImage;
+
+    public float ultCooldownTimer;
+    public float ultCooldownValue = 10.0f;
+    public bool ultTriggerCooldown;
+
+    public float ultTarget;
+    public float ultReduceSpeed = 2f;
+
+    Color ultCooldownColor = new Color32(164, 164, 164, 255);
+    //Color ultDefaultColor = new Color32(255, 255, 255, 168);
+    Color ultDefaultColor = new Color32(255, 150, 150, 168);
+
+
+
 
     private void Start()
     {
         ResetCooldown();
+        UltResetCooldown();
     }
 
     private void Update()
@@ -36,12 +57,18 @@ public class PlayerCooldowns : MonoBehaviour
             triggerCooldown = true;
         }
 
+
+        if (Input.GetButtonDown("Ult"))
+        {
+            ultTriggerCooldown = true;
+        }
+
     }
 
     private void FixedUpdate()
     {
         TriggerCooldown();
-
+        UltTriggerCooldown();
     }
 
     public void TriggerCooldown()
@@ -68,6 +95,43 @@ public class PlayerCooldowns : MonoBehaviour
                 ResetCooldown();
             }
         }
+    }
+
+    public void UltTriggerCooldown()
+    {
+        if (ultTriggerCooldown)
+        {
+            ultCooldownTimer -= Time.deltaTime;
+
+            int numberOfDecimals = 1;
+
+            float convertedCooldownTimer = Mathf.Round(ultCooldownTimer * Mathf.Pow(10, numberOfDecimals)) / Mathf.Pow(10, numberOfDecimals);
+
+            ultIconImage.color = ultCooldownColor;
+
+            //iconImage.gameObject.SetActive(false);
+            //foregroundImage.gameObject.SetActive(false);
+            ultForegroundImage.fillAmount = ultCooldownTimer / ultCooldownValue;
+
+            ultCooldownText.gameObject.SetActive(true);
+            ultCooldownText.text = convertedCooldownTimer + "";
+
+            if (ultCooldownTimer <= 0)
+            {
+                UltResetCooldown();
+            }
+        }
+    }
+
+    public void UltResetCooldown()
+    {
+        ultIconImage.color = ultDefaultColor;
+        ultIconImage.gameObject.SetActive(true);
+        ultTriggerCooldown = false;
+        ultCooldownTimer = ultCooldownValue;
+        ultTarget = ultCooldownTimer / ultCooldownValue;
+        ultCooldownText.gameObject.SetActive(false);
+        ultForegroundImage.gameObject.SetActive(true);
     }
 
     public void ResetCooldown()
