20765c9a639ba376da5ce06db846d1adf2e02014
Kaue Godoy <105384509+KaueGodoy@users.noreply.github.com>
2023-10-26 22:27:41 -0300

    NewProps

---
+++ Roguelike/Assets/Scripts/Interactable/Interactable_Props/Chest/InteractableObject_AddToInventory.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject_AddToInventory : Interactable
+{
+    [SerializeField] private string _name;
+    [SerializeField] private List<string> drops = new List<string>();
+
+    public override void Awake()
+    {
+        base.Awake();
+        Name = _name;
+    }
+
+    public List<string> Drops
+    {
+        get { return drops; }
+        set { drops = value; }
+    }
+
+    public override void ExecuteInteraction()
+    {
+        base.ExecuteInteraction();
+        OpenChest();
+        DestroyGameObject();
+    }
+
+    public virtual void OpenChest()
+    {
+        if (Drops == null) return;
+
+        foreach (var item in Drops)
+        {
+            GiveItemToPlayer(item);
+        }
+
+        AudioManager.PlaySound("OnChestOpen");
+    }
+}
