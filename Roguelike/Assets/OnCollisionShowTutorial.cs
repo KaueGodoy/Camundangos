f7be3cdaab3bc91bb74e6922f5abb9acbf47182a
Kaue Godoy <105384509+KaueGodoy@users.noreply.github.com>
2024-04-04 09:38:54 -0300

    TutorialSystem

---
+++ Roguelike/Assets/OnCollisionShowTutorial.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class OnCollisionShowTutorial : MonoBehaviour
+{
+    private GameObject _tutorialText;
+
+    private void Start()
+    {
+        _tutorialText = transform.Find("TutorialText").gameObject;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (_tutorialText != null)
+            {
+                _tutorialText.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) 
+        {
+            if (_tutorialText != null)
+            {
+                _tutorialText.SetActive(false);
+            }
+        }
+    }
+}
