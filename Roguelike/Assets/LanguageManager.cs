6212276fef9681d039c607d68139d4a67c9a8355
Kaue Godoy <105384509+KaueGodoy@users.noreply.github.com>
2024-04-30 08:10:41 -0300

    LocalizationFix+ParallaxReseter

--- Roguelike/Assets/LanguageManager.cs
+++ Roguelike/Assets/LanguageManager.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
+using UnityEngine.UI;
 
 public class LanguageManager : MonoBehaviour
 {
+    public static LanguageManager Instance { get; private set; }
+
+    [SerializeField] private Button _englishLanguageButton;
+    [SerializeField] private Button _brazilianPortugueseLanguageButton;
+    [SerializeField] private Button _japaneseLanguageButton;
+
     private bool _isActive = false;
 
     private string _localeKey = "LocaleKey";
-    private int _defaultLocaleId = 0;
+    private int _currentLocaleId = 0;
+
+    private int _englishLocaleId = 0;
+    private int _brazilianPortugueseLocaleId = 1;
+    private int _japaneseLocaleId = 2;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        _englishLanguageButton.onClick.AddListener(() =>
+        {
+            ChangeLocaleId(_englishLocaleId);
+        });
+
+        _brazilianPortugueseLanguageButton.onClick.AddListener(() =>
+        {
+            ChangeLocaleId(_brazilianPortugueseLocaleId);
+        });
+
+        _japaneseLanguageButton.onClick.AddListener(() =>
+        {
+            ChangeLocaleId(_japaneseLocaleId);
+        });
+
+        //if (Instance == null)
+        //{
+        //    Instance = this;
+        //}
+        //else
+        //{
+        //    Destroy(gameObject);
+        //    return;
+        //}
+
+        //DontDestroyOnLoad(gameObject);
+    }
 
     private void Start()
     {
-        int id = PlayerPrefs.GetInt(_localeKey, _defaultLocaleId);
+        int id = PlayerPrefs.GetInt(_localeKey, _currentLocaleId);
         ChangeLocaleId(id);
+        Debug.Log("Locale id: " + _currentLocaleId);
     }
 
     public void ChangeLocaleId(int localeId)
@@ -20,6 +64,8 @@ public class LanguageManager : MonoBehaviour
         if (_isActive) return;
 
         CallSetLocaleCoroutine(localeId);
+        Debug.Log("Locale id: " + _currentLocaleId);
+
     }
 
     private void CallSetLocaleCoroutine(int localeId)
@@ -35,6 +81,4 @@ public class LanguageManager : MonoBehaviour
         PlayerPrefs.SetInt(_localeKey, localeId);
         _isActive = false;
     }
-
-
 }
